2ff84d50a03fa1df30c40505f3cdafef91856724
indesperate <indespreate@outlook.com>
2022-12-03 17:22:12 +0800

    finish lab1

--- lab/lab01/exercise4/ll_cycle.c
+++ lab/lab01/exercise4/ll_cycle.c
@@ -1,6 +1,36 @@
 #include <stddef.h>
+#include <stdbool.h>
 #include "ll_cycle.h"
 
-int ll_has_cycle(node *head) {
-    /* TODO: Implement ll_has_cycle */
+bool ptr_add_by_one(node **ptr)
+{
+    if (*ptr == NULL)
+    {
+        return false;
+    }
+    *ptr = (*ptr) -> next;
+    return true;
+}
+
+bool ptr_add_by_two(node **ptr)
+{
+    if (ptr_add_by_one(ptr))
+    {
+        return ptr_add_by_one(ptr);
+    }
+    return false;
+}
+
+int ll_has_cycle(node *head)
+{
+    node *fast_ptr = head;
+    node *slow_ptr = head;
+    while (ptr_add_by_two(&fast_ptr) && ptr_add_by_one(&slow_ptr))
+    {
+        if (fast_ptr == slow_ptr)
+        {
+            return true;
+        }
+    }
+    return false;
 }
