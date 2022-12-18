6be69f470b65cdc38e9346c481f31a9f8fc0e304
indesperate <indespreate@outlook.com>
2022-12-18 18:27:07 +0800

    finish lab4
    finish ex2
    
    finish lab4

--- lab/lab04/discrete_fn.s
+++ lab/lab04/discrete_fn.s
@@ -100,7 +100,10 @@ main:
 # a0 is the value we want to evaluate f at
 # a1 is the address of the "output" array (defined above).
 f:
-    # YOUR CODE GOES HERE!
+    addi a0, a0, 3 # set offset = 3 word
+    slli a0, a0, 2 # set real offset
+    add a0, a0, a1 # get address
+    lw a0, 0(a0) # load word
 
     jr ra               # Always remember to jr ra after your function!
 
